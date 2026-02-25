3a8c729a5f0ef98690cfb8c466591ee7cae9c69d
FastmanTheDuke <support@mdxp.io>
2026-02-25 10:18:19 +0100

    Translations

---
+++ includes/translation.php
@@ -0,0 +1,29 @@
+<?php
+// Ensure Polylang is active
+if (function_exists('pll_register_string')) {
+    // Register translations
+    pll_register_string('lire', 'lire', 'Theme Translations');
+    pll_register_string('retour', 'retour', 'Theme Translations');
+    pll_register_string('Rédigé par', 'Rédigé par', 'Theme Translations');
+    pll_register_string('Documents', 'Documents', 'Theme Translations');
+    pll_register_string('Liens', 'Liens', 'Theme Translations');
+    pll_register_string('herozone-actualite', 'herozone-actualite', 'Theme Translations');
+    pll_register_string('herozone-heritage', 'herozone-heritage', 'Theme Translations');
+    pll_register_string('herozone-evenement', 'herozone-evenement', 'Theme Translations');
+    pll_register_string('herozone-marque', 'herozone-marque', 'Theme Translations');
+    pll_register_string('herozone-actualites', 'herozone-actualites', 'Theme Translations');
+    pll_register_string('herozone-evenements', 'herozone-evenements', 'Theme Translations');
+
+    pll_register_string('Decouvrir', 'Decouvrir', 'Theme Translations');
+    pll_register_string('focus', 'focus', 'Theme Translations');
+    pll_register_string('past event', 'past event', 'Theme Translations');
+    pll_register_string('coming soon', 'coming soon', 'Theme Translations');
+    pll_register_string('Tous nos événements', 'Tous nos événements', 'Theme Translations');
+    pll_register_string('Toutes nos actualités', 'Toutes nos actualités', 'Theme Translations');
+    pll_register_string('En savoir plus', 'En savoir plus', 'Theme Translations');
+    pll_register_string('Tous droits réservés', 'Tous droits réservés', 'Theme Translations');
+    pll_register_string('voir tout', 'voir tout', 'Theme Translations');
+    pll_register_string('Charger plus de publications', 'Charger plus de publications', 'Theme Translations');
+    pll_register_string('Nos évènements passés', 'Nos évènements passés', 'Theme Translations');
+}
+?>
