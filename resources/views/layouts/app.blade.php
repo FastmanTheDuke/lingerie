9abfacab15dd8c95818b011117424185d5af4724
FastmanTheDuke <support@mdxp.io>
2026-02-25 12:39:39 +0100

    Add files via upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,34 @@
- <!doctype html>
+<!doctype html>
 <html @php(language_attributes())>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    @php(do_action('get_header'))
-    @php(wp_head())
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-  </head>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @php(do_action('get_header'))
+  @php(wp_head())
 
-  <body @php(body_class())>
-    @php(wp_body_open())
-    <div id="custom-cursor"><div class="round"><span class="text"></span></div><span></span></div>
-    <div id="app">
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-      @include('sections.header')
+<body @php(body_class())>
+  @php(wp_body_open())
+  <div id="custom-cursor">
+    <div class="round"><span class="text"></span></div><span></span>
+  </div>
+  <div id="app">
 
-      <main id="main" class="main">
-        @yield('content')
-      </main>
+    @include('sections.header')
 
-      @include('sections.footer')
-    </div>
+    <main id="main" class="main">
+      @yield('content')
+    </main>
 
-    @php(do_action('get_footer'))
-    @php(wp_footer())
-  </body>
+    @include('sections.footer')
+  </div>
+
+  @php(do_action('get_footer'))
+  @php(wp_footer())
+  @include('partials.popup-auth')
+</body>
+
 </html>
