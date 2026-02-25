1ac8aaab0c112c033f177d19702e5764e503d856
FastmanTheDuke <support@mdxp.io>
2026-02-25 12:49:26 +0100

    Add files via upload

--- resources/views/components/card-mode.blade.php
+++ resources/views/components/card-mode.blade.php
@@ -4,11 +4,15 @@
   'tag' => null,
   'url' => null,
   'date' => null,
+  'is_private' => null,
+  'has_access' => null,
 ])
 
-<div class="relative ajax-card group" data-cursor-type="mode" data-cursor-text="{{pll_e('lire')}}" data-cursor-rotate="25">
-  @if($url)
-    <a href="{{ $url }}" class="!no-underline">
+<div class="relative ajax-card group private_{{ $is_private }} has_access_{{ $has_access }} {{ $is_private && !$has_access ? 'js-private-post' : '' }}" data-cursor-type="mode" data-cursor-text="{{pll_e('lire')}}" data-cursor-rotate="25">
+  @if($url && (!$is_private || $has_access))
+      <a href="{{ $url }}" class="!no-underline">
+    @else
+      <div class="cursor-pointer js-open-auth-popup">
   @endif
   @if($visual)
     <div class="w-full overflow-hidden bg-[#F4F2EF]">
@@ -20,9 +24,18 @@
       
   @endif
   <div class="py-6 flex flex-col gap-6 max-sm:gap-4">
+
       @if($tag)
           <div class="content-type3 text-dark flex gap-2 items-center">
             <div class="w-2 h-2 bg-black rounded-full"></div>
+            @if($is_private && !$has_access)
+                <div class="restricted-content">
+                    <p>Ce contenu est privé. Veuillez vous connecter via le lien envoyé par email.</p>
+                    {{-- Afficher ici un résumé ou le flou --}}
+                </div>
+            @else
+               
+            @endif
             <p>{{ $tag }}</p>
           </div>
       @endif
@@ -34,8 +47,10 @@
           <p class="content-type2 text-lightGrey">{{ $date }}</p>
       @endif
   </div>
-  @if($url)
+  @if($url && (!$is_private || $has_access))
     </a>
+  @else
+    </div>
   @endif
 </div>
 
