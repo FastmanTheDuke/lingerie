12b5b858db886530fa8c77ef6689a476d535d99e
FastmanTheDuke <support@mdxp.io>
2026-02-25 13:32:02 +0100

    Add files via upload

--- resources/views/single-mode.blade.php
+++ resources/views/single-mode.blade.php
@@ -3,5 +3,13 @@
 @section('content')
 
     @include('partials.default.herozone', ['title' => $title, 'surtitle' => $parentPostType, 'visual_herozone' => $visual_herozone, 'tags' => $tags, 'date' => $date])
-    @include('partials.content-blocks-fiche')
+    @if($is_private && !$has_access)
+        <div class="container py-20 text-center">
+            <h2 class="font-forum text-3xl mb-4">Contenu Privé</h2>
+            <p>Veuillez utiliser le lien d'accès envoyé par email pour consulter cette fiche mode.</p>
+            <button class="js-open-auth-popup underline mt-4">Recevoir un lien d'accès</button>
+        </div>
+    @else
+        @include('partials.content-blocks-fiche')
+    @endif
 @endsection
