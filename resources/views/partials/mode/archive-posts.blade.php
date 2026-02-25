23b3b57b4e50cfce9e0f8923b6ec4d51fad35cf8
FastmanTheDuke <support@mdxp.io>
2026-02-25 12:39:02 +0100

    Add files via upload

--- resources/views/partials/mode/archive-posts.blade.php
+++ resources/views/partials/mode/archive-posts.blade.php
@@ -2,7 +2,8 @@
 
     @foreach($posts as $post)
         <x-card-mode visual="{{$post['visual']}}" titre="{!!$post['title']!!}" tag="{{$post['tag']}}"
-            url="{{$post['permalink']}}" date="{{$post['date']}}" />
+            url="{{$post['permalink']}}" date="{{$post['date']}}" is_private="{{$post['is_private']}}"
+            has_access="{{$post['has_access']}}" />
     @endforeach
 
 @else
