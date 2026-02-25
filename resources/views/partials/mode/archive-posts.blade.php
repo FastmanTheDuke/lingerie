7ce7f04fae44d4ca6e6bb41cce8f1a2b6a5f1b8f
FastmanTheDuke <support@mdxp.io>
2026-02-25 13:31:45 +0100

    Add files via upload

--- resources/views/partials/mode/archive-posts.blade.php
+++ resources/views/partials/mode/archive-posts.blade.php
@@ -1,11 +1,8 @@
-@if($posts)
-
-    @foreach($posts as $post)
+{{-- Dans resources/views/partials/mode/archive-posts.blade.php --}}
+@if($posts && isset($posts['list']))
+    @foreach($posts['list'] as $post) {{-- Ajoutez ['list'] ici --}}
         <x-card-mode visual="{{$post['visual']}}" titre="{!!$post['title']!!}" tag="{{$post['tag']}}"
-            url="{{$post['permalink']}}" date="{{$post['date']}}" is_private="{{$post['is_private']}}"
-            has_access="{{$post['has_access']}}" />
+            url="{{$post['permalink']}}" date="{{$post['date']}}" is_private="{{$post['is_private']}}" {{-- Sera enfin rempli
+            --}} has_access="{{$post['has_access']}}" />
     @endforeach
-
-@else
-    <p>Aucun résultat trouvé.</p>
 @endif
